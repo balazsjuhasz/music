2498635f60deef359a0c17aa11585eeddbf3c4de
Balazs Juhasz <juhaszbal@gmail.com>
2023-10-31 00:03:19 +0100

    Create action for register

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -103,7 +103,8 @@
 </template>
 
 <script>
-import { auth, usersCollection } from "@/includes/firebase";
+import { mapActions } from "pinia";
+import useUserStore from "@/stores/user";
 
 export default {
   name: "RegisterForm",
@@ -128,29 +129,17 @@ export default {
     };
   },
   methods: {
+    ...mapActions(useUserStore, {
+      createUser: "register",
+    }),
     async register(values) {
       this.reg_show_alert = true;
       this.reg_in_submission = true;
       this.reg_alert_variant = "bg-blue-500";
       this.reg_alert_message = "Please wait! Your account is being created.";
 
-      let userCred = null;
       try {
-        userCred = await auth.createUserWithEmailAndPassword(values.email, values.password);
-      } catch (error) {
-        this.reg_in_submission = false;
-        this.reg_alert_variant = "bg-red-500";
-        this.reg_alert_message = "An unexpected error occured. Please try again later.";
-        return;
-      }
-
-      try {
-        await usersCollection.add({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country,
-        });
+        await this.createUser(values);
       } catch (error) {
         this.reg_in_submission = false;
         this.reg_alert_variant = "bg-red-500";
@@ -160,7 +149,6 @@ export default {
 
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_message = "Success! Your account has been created.";
-      console.log(userCred);
     },
   },
 };
