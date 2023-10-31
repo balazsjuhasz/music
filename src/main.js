4c4d42040ca37ee65c1e37af002dedf8d7be1f7c
Balazs Juhasz <juhaszbal@gmail.com>
2023-10-31 11:11:57 +0100

    Finish registration form

--- src/main.js
+++ src/main.js
@@ -1,18 +1,23 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import VeeValidatePlugin from "./includes/validation";
+import { auth } from "./includes/firebase";
+
 import "./assets/base.css";
 import "./assets/main.css";
 
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import VeeValidatePlugin from "./includes/validation";
-import "./includes/firebase";
+let app;
 
-import App from "./App.vue";
-import router from "./router";
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App);
 
-const app = createApp(App);
+    app.use(createPinia());
+    app.use(router);
+    app.use(VeeValidatePlugin);
 
-app.use(createPinia());
-app.use(router);
-app.use(VeeValidatePlugin);
-
-app.mount("#app");
+    app.mount("#app");
+  }
+});
