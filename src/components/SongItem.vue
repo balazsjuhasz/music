28e496efa38ede78dd41355921d34627b125db22
Balazs Juhasz <juhaszbal@gmail.com>
2023-11-06 10:30:35 +0100

    Implement music player
    Finish music player. Add transition between pages

--- src/components/SongItem.vue
+++ src/components/SongItem.vue
@@ -3,7 +3,7 @@
     class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50"
   >
     <div>
-      <router-link
+      <RouterLink
         :to="{
           name: 'song',
           params: {
@@ -13,15 +13,27 @@
         class="font-bold block text-gray-600"
       >
         {{ song.modified_name }}
-      </router-link>
+      </RouterLink>
       <span class="text-gray-500 text-sm">{{ song.display_name }}</span>
     </div>
 
     <div class="text-gray-600 text-lg">
-      <span class="comments">
-        <i class="fa fa-comments text-gray-600"></i>
-        {{ song.comment_count }}
-      </span>
+      <RouterLink
+        custom
+        :to="{
+          name: 'song',
+          params: {
+            id: song.docID,
+          },
+          hash: '#comments',
+        }"
+        v-slot="{ navigate }"
+      >
+        <span class="comments" @click="navigate">
+          <i class="fa fa-comments text-gray-600"></i>
+          {{ song.comment_count }}
+        </span>
+      </RouterLink>
     </div>
   </li>
 </template>
