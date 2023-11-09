9736e7cb398c41add19cb24e0eda99b0e7130081
Balazs Juhasz <juhaszbal@gmail.com>
2023-11-09 12:02:31 +0100

    Initial i18n

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -32,6 +32,13 @@
             </li>
           </template>
         </ul>
+        <ul class="ml-auto">
+          <li>
+            <a class="px-2 text-white" href="#" @click.prevent="changeLocale">
+              {{ currentLocale }}
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -46,6 +53,9 @@ export default {
   name: "AppHeader",
   computed: {
     ...mapStores(useModalStore, useUserStore),
+    currentLocale() {
+      return this.$i18n.locale === "hu" ? "Hungarian" : "English";
+    },
   },
   methods: {
     toggleAuthModal() {
@@ -57,6 +67,9 @@ export default {
       if (this.$route.meta.requiresAuth) {
         this.$router.push({ name: "home" });
       }
+    },
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale === "hu" ? "en" : "hu";
     },
   },
 };
